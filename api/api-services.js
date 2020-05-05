c35edd087b1e7ce28f294b14d6327d9841e8a914
Léa <lofficial.lea.m@gmail.com>
2020-05-05 16:43:43 +0200

    Index + api + api services

---
+++ api/api-services.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+
+module.exports = class PeopleService {
+    constructor() {
+        this.peoples = JSON.parse(fs.readFileSync(__dirname + '/people.json', 'utf8'));
+    }
+
+    updatePeople(id, people) {
+        let perId = this.peoples.indexOf(this.peoples.filter(person => person.id = id))
+        this.peoples[perId] = people;
+        fs.writeFile(__dirname + '/people.json', JSON.stringify(this.peoples), function writeJSON(err) {
+            if (err) return console.log(err);
+        });
+    }
+
+    getPeople(filters = null) {
+        // To be implemented!
+        if (filters === null) {
+            return this.peoples;
+        } else {
+            return this.peoples.filter(filters);
+        }
+    }
+}
